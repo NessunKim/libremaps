0615d5d417ba6924cb52c2cbd1c87af51859935e
Nessun Kim <nessunkim@gmail.com>
2021-02-22 01:14:28 +0900

    Fix markers api

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 extern crate serde_derive;
-// #[macro_use]
-// extern crate jsonapi;
 pub mod routes;
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 
 pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(routes::markers::get))
-        .bind("127.0.0.1:8081")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default().allowed_origin("http://localhost:8080");
+        App::new().wrap(cors).service(routes::markers::get)
+    })
+    .bind("127.0.0.1:8081")?
+    .run()
+    .await
 }
--- src/routes/markers.rs
+++ src/routes/markers.rs
@@ -10,27 +10,30 @@ struct Marker {
     name: String,
     latitude: f32,
     longitude: f32,
+    zoom: i32,
 }
 jsonapi_model!(Marker; "markers");
 
 #[derive(Deserialize)]
 struct MarkersGetQuery {
-    left: f32,
-    right: f32,
-    top: f32,
-    bottom: f32,
+    west: f32,
+    east: f32,
+    north: f32,
+    south: f32,
+    zoom: i32,
 }
 
 #[get("/markers")]
 async fn get(query: Query<MarkersGetQuery>) -> impl Responder {
     let mut rng = rand::thread_rng();
     let mut markers = vec![];
-    for i in 1..30 {
+    for i in 1..=30 {
         let marker = Marker {
             id: format!("{}", i),
             name: format!("Example{}", i),
-            latitude: rng.gen_range(query.top..query.bottom),
-            longitude: rng.gen_range(query.left..query.right),
+            latitude: rng.gen_range(query.south..query.north),
+            longitude: rng.gen_range(query.west..query.east),
+            zoom: rng.gen_range(0..=query.zoom),
         };
         markers.push(marker)
     }
